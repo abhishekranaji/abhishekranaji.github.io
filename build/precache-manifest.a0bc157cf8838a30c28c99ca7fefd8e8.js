6689462aa687d234cd112ccb3c47c73558ac165a
Abhishek Rana <53045241+abhishekranaji@users.noreply.github.com>
2020-10-23 22:35:38 +0530

    Add files via upload

---
+++ build/precache-manifest.a0bc157cf8838a30c28c99ca7fefd8e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fae34ef14238aa36116b44af770a55a",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "c777a0affb141037f958",
+    "url": "/build/static/css/main.c3c8ab23.chunk.css"
+  },
+  {
+    "revision": "515e1b93d5c11112c0b8",
+    "url": "/build/static/js/2.0fc145f2.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "/build/static/js/2.0fc145f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c777a0affb141037f958",
+    "url": "/build/static/js/main.4b403f21.chunk.js"
+  },
+  {
+    "revision": "c19cfcee9d9098580d91",
+    "url": "/build/static/js/runtime-main.85a78c74.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0793b99d895373a6ed0daf9a7ff9b0c1.js"
+  "/build/precache-manifest.a0bc157cf8838a30c28c99ca7fefd8e8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
